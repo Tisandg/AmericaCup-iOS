0059738c30fc51740359b5d62d0037b24490c5ba
Santiago Garcia <santiagogarcia@santiagos-mac.local>
2019-07-03 15:34:35 -0700

    Initial Commit

---
+++ AmericaCup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmericaCup
+//
+//  Created by Santiago Garcia on 7/3/19.
+//  Copyright © 2019 Santiago Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
