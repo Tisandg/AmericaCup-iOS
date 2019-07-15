3dfcc7e17c201d4c74d119820a0496db728168b2
Santiago Garcia <santiagogarcia@santiagos-mac.local>
2019-07-14 21:30:07 -0500

    add scene about team detail

--- AmericaCup/GroupTableViewController.swift
+++ AmericaCup/GroupTableViewController.swift
@@ -24,49 +24,30 @@ class GroupTableViewCell: UITableViewCell {
 class GroupTableViewController: UITableViewController {
     
     var groupManager:GroupManager = GroupManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return groupManager.groupCount
     }
-    
-    //Sets the cell look and feel and data in the table row
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Configure the cell...
-        
-        /*It first checks for any cells with the requested reuse identifier in the cache, and if none are found,
-         it creates a new cell. When a cell is scrolled offscreen, rather than removing the cell from memory,
-         it's sent to the cache to be reused*/
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath) as! GroupTableViewCell
         
-        /*IndexPath parameter contains the number of the section and the row of the cell it’s interested in. It then
-         gets a table view cell for this index path, based on the reuse identifier you defined earlier in Interface Builder.
-         The index path is generally useful here to know what data to inject into the cell*/
         let group = groupManager.getGroup(at: indexPath.row)
-        
-        //Set the text, label an image for each according to the format design we chose for the cell
+
         cell.nameGroup?.text = group.nameGroup
         
         cell.team1?.text = group.teams[0].nombre
